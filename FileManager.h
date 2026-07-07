f833f9cbef904f1d3f63c49233ba2266c55c6d67
rajatpandeyksj-crypto <rajatpandeyksj@gmail.com>
2026-07-07 22:57:25 +0530

    Add function declarations for contact management

--- FileManager.h
+++ FileManager.h
@@ -1,1 +1,22 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
 
+#include "Contact.h"
+
+// Function Declarations
+
+void addContact();
+
+void displayContacts();
+
+void searchContact();
+
+void updateContact();
+
+void deleteContact();
+
+void saveContacts();
+
+void loadContacts();
+
+#endif
