776fbef1e1b75878558c12eb718de03165db09db
rajatpandeyksj-crypto <rajatpandeyksj@gmail.com>
2026-07-10 10:29:49 +0530

    Update Contact.h

--- Contact.h
+++ Contact.h
@@ -1,11 +1,25 @@
-#include <iostream>
-using namespace std;
+#ifndef CONTACT_H
+#define CONTACT_H
 
-struct Contact
-{
-    string name;
-    string phone;
-    string email;
-    string address;
+#include <string>
+#include <vector>
+
+// Structure mapping out a single contact's details
+struct Contact {
+    std::string name;
+    std::string phone;
+    std::string email;
+    std::string address;
 };
 
+// Shares the database array dynamically across files
+extern std::vector<Contact> contacts;
+
+// Core logic function declarations
+void addContact();
+void displayContacts();
+void searchContact();
+void updateContact();
+void deleteContact();
+
+#endif
